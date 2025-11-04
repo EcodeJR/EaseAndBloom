19c37c85c8a509f81f6c8b15f615275a13a53312
EcodeJR <emmanueldcode@gmail.com>
2025-11-04 11:24:14 +0100

    Implemented new email service.

--- AdminControl/backend/utils/email.js
+++ AdminControl/backend/utils/email.js
@@ -3,7 +3,9 @@ const nodemailer = require('nodemailer');
 // Create email transporter
 const createTransporter = () => {
   return nodemailer.createTransport({
-    service: process.env.EMAIL_SERVICE,
+    host: process.env.EMAIL_SERVICE,
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
